17a382b9b4f1b2be726ea20f6264082ca94976d6
askold <vkramar.biz@gmail.com>
2026-04-18 02:52:09 +0300

    small change.

--- back-end/KramarDev.Quiz.WebAPI/Model/LoginModel.cs
+++ back-end/KramarDev.Quiz.WebAPI/Model/LoginModel.cs
@@ -1,8 +1,14 @@
-﻿namespace KramarDev.Quiz.WebAPI.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KramarDev.Quiz.WebAPI.Model;
 
 public sealed record LoginModel
 {
+    [Required]
+    [StringLength(32, MinimumLength = 3)]
     public string Username { get; init; }
 
+    [Required]
+    [StringLength(32, MinimumLength = 6)]
     public string Password { get; init; }
 }
