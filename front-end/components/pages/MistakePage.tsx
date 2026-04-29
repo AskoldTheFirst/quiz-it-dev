d4c06f966d65b6c4871c322e1443e9f811c2559a
vkramar <vkramar.biz@gmail.com>
2026-04-29 14:00:24 +0300

    correcting UX.

--- front-end/components/pages/MistakePage.tsx
+++ front-end/components/pages/MistakePage.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
 import Mistakes from "../quiz/Mistakes";
-import { resetFilters } from "@/redux/mistakesSlice";
-import { useAppDispatch } from "@/redux/store";
 
 export default function MistakePage() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(resetFilters());
-  }, [dispatch]);
-
   return <Mistakes />;
 }
